06d00b57d14abf9dbd7a88d16a09cb0e8de89683
zkj124578963 <1809400941@qq.com>
2025-12-05 22:18:58 +0800

    2025年12月5日：默认开启所有规则

--- src/apps/com.b1585bcce5.v174991545.o81dcc058a20251204.ts
+++ src/apps/com.b1585bcce5.v174991545.o81dcc058a20251204.ts
@@ -2,7 +2,7 @@ import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
   id: 'com.b1585bcce5.v174991545.o81dcc058a20251204',
-  name: '瓜子影视 v3.0.3.4',
+  name: '瓜子影视  v3.0.3.4',
   groups: [
     {
       key: 3,
